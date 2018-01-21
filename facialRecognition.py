0252d392c182e605757aee37c06435459659e5ca
HechenHu <XetRATomHu@hotmail.com>
2018-01-20 23:33:57 -0500

    asd

--- facialRecognition.py
+++ facialRecognition.py
@@ -2,20 +2,26 @@ import cv2
 import os
 import numpy as np
 
-#Turn a image into gray scale
+subject = ["Nicholas_Cage", "Jacksfilms"]
+
+#Turn an image into gray scale
 def grayConversion(img):
-    return gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return gray
 
 
 
 
 #Return a list of detected faces in the gray scale
-def detectFace(img):
+def detectFace(folder_path, img):
     #Convert image to gray scale
-    gray = grayConversion(img)
+    os.chdir(folder_path)
+    image = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+#   temp = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = np.array(image, 'uint8')
     #load OpenCV face detector(LBP)
-    face_cascade = cv2.CascadeClassifier('opencv-files/lbpcascade_frontalface.xml')
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);
+    face_cascade = cv2.CascadeClassifier('/usr/share/opencv/lbpcascades/lbpcascade_frontalface.xml')
+    faces = face_cascade.detectMultiScale(gray);
     processed_faces = []
     if faces is not None:
         for (x,y,w,h) in faces:
@@ -27,33 +33,37 @@ def detectFace(img):
 def prepareTrainingData(profile_data_folder_path):
     faces = []
     labels = []
+    os.chdir(profile_data_folder_path)
     for folder_name in os.listdir(profile_data_folder_path):
-        label = folder_name
+        os.chdir(profile_data_folder_path + "/" + folder_name)
+        label = int(folder_name)
         folder_path = profile_data_folder_path + "/" + folder_name
         image_names = os.listdir(folder_path)
-         for image_name in image_names:
-             #Ignore system files
-            if image_name.startwith("."):
-                continue;
-                image_path = profile_data_folder_path + "/" + image_name
-                image = cv2.imread(image_path)
-                face = detectFace(image)
+        for image_name in image_names:
+            #Ignore system files
+            if not image_name.startswith("."):
+                os.chdir(folder_path)
+                face = detectFace(folder_path, image_name)
+                temp = np.array(face, 'uint8')
                 if face is not None:
-                    faces.append(face)
+                    faces.append(temp[0])
                     labels.append(label)
-     return faces, labels
-face_recognzer = cv2.face.createLBPHFaceRecognzer()
+    os.chdir('..')
+    return faces, labels
+
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 #Train recognizer
 def trainRecognizer(profile_data_folder_path):
     faces, labels = prepareTrainingData(profile_data_folder_path)
+    for item in faces:
+        item = np.array(item, 'uint8')
+    print(labels)
     face_recognizer.train(faces, np.array(labels))
 
 
-#Matching function. Return a list of matched labels.
-def match(input_image):
-    face = detectFace(input_image)
-    label = face_recognizer.predict(face)
-    return label
-
-
+#Matching function. Return a list of matched labels
+def match(directory, input_image):
+        face = detectFace(directory, input_image)
+        label = face_recognizer.predict(face[0])
+        return label
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 #Main.py
 import facialRecognition as fR
-import #SQL Module
+import coredatabase as cdb
+import time
+import os
 
-f = file(ctOS.conf)
-profile_folder_path = ""
-for line in f:
-    if '#' in line:
-        continue;
-    profile_folder_path.append(line)
+profile_folder_path = os.getcwd() + "/profile_images"
+cdb.create_table("profiles", "name TEXT, age INTEGER, birthdate TEXT, crime TEXT, priority INTEGER, images_path TEXT")
+cdb.create_table("matching_logs", "time TEXT, location TEXT, name TEXT")
+fR.trainRecognizer(profile_folder_path)
+os.chdir('..')
+for input_image_name in os.listdir(os.getcwd() + "/raw_images"):
+    matched = fR.match(os.getcwd() + "/raw_images", input_image_name)
+    for match in matched:
+        local_time = time.asctime(time.localtime(time.time()))
+        cdb.add_matching_logs(local_time, "BI", match)
+    shutil(os.getcwd() + "raw_images/" + input_image_name, os.getcwd() + "processed_images/" + input_image_name)
 
-
+for lo in location_logs("BI"):
+    print(lo)
 
